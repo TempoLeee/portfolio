e372ca5c0f8c85fa85edda3a810e58c17e32552b
Tempo Lee <tempoleekk@gmail.com>
2024-01-24 01:27:22 +0800

    Add

---
+++ scripts.js
@@ -0,0 +1,8 @@
+var side_menu = document.getElementById("side_menu");
+
+function open_menu() {
+    side_menu.style.right = "0";
+}
+function close_menu() {
+    side_menu.style.right = "-200px";
+}
